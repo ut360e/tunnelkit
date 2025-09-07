db835f366e7a1a005cb9ad9ab6bdaf74e7144483
Md. Ehsanul Hoque <showrav017@gmail.com>
2025-09-07 20:02:17 +0600

    openssl-apple updated

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.3.3001"),
+        .package(url: "https://github.com/google/grpc-binary.git", from: "1.62.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -118,7 +118,7 @@ let package = Package(
             dependencies: [
                 "CTunnelKitCore",
                 "CTunnelKitOpenVPNCore",
-                "OpenSSL"
+                "grpc-binary"
             ]),
         .target(
             name: "__TunnelKitUtils",
