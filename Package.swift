a30a96d3f357371508f74188101787cc38f2513c
Md. Ehsanul Hoque <showrav017@gmail.com>
2025-09-07 19:38:49 +0600

    openssl-apple updated

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.0"),
-        //.package(url: "https://github.com/passepartoutvpn/openssl-apple", from: "3.5.200"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", from: "3.3.3001"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -118,7 +118,7 @@ let package = Package(
             dependencies: [
                 "CTunnelKitCore",
                 "CTunnelKitOpenVPNCore",
-                "openssl-apple"
+                "OpenSSL"
             ]),
         .target(
             name: "__TunnelKitUtils",
